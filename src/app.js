8d5e0dd130c63c4473c951a6399765471004de79
tashinmustakim-39 <tashinmustakim@iut-dhaka.edu>
2026-01-24 18:49:59 +0600

    add routes and controllers structure

--- src/app.js
+++ src/app.js
@@ -1,25 +1,20 @@
 const express = require('express');
 const cors = require('cors');
 
+const testRoutes = require('./routes/test.routes');
+
 const app = express();
 
-// middleware
 app.use(cors());
 app.use(express.json());
 
-// test routes
 app.get('/', (req, res) => {
   res.json({
-    message: 'Hotel Management API is running',
-    env: process.env.NODE_ENV
+    message: 'Hotel Management API is running'
   });
 });
 
-app.get('/api/test', (req, res) => {
-  res.json({
-    success: true,
-    message: 'Test route is working'
-  });
-});
+// use routes
+app.use('/api', testRoutes);
 
 module.exports = app;
---
+++ src/controllers/test.controller.js
@@ -0,0 +1,6 @@
+exports.test = (req, res) => {
+  res.json({
+    success: true,
+    message: 'Controller + route structure working'
+  });
+};
