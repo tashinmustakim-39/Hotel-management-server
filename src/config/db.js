c072754801bb946ba290772b273b01cbc7601747
tashinmustakim-39 <tashinmustakim@iut-dhaka.edu>
2026-01-24 20:40:02 +0600

    move DB config file to .env file

--- src/config/db.js
+++ src/config/db.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2');
 
 const db = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '220041239',
-  database: 'hotel_management',
-  port: 3306
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
 
 module.exports = db;
